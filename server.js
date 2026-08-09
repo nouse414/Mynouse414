507dcc2fc64fe8795b8f9dc36cad41d15d50c417
nouse414 <nouse414@proton.me>
2026-08-08 23:10:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("My Nuvio addon is running!");
+});
+
 app.get("/manifest.json", (req, res) => {
   res.json({
-    id: "com.mynouse414.nuvio",
+    id: "com.mynuvio.addon",
     version: "1.0.0",
-    name: "My Nuvio P2P Addon",
-    description: "Personal addon for authorized media",
+    name: "My Nuvio Addon",
+    description: "Personal addon",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"]
   });
 });
-const PORT = process.env.PORT || 3000;
+
+app.get("/stream/:type/:id.json", (req, res) => {
+  res.json({
+    streams: []
+  });
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Addon running on port " + PORT);
-});
-
-app.listen(PORT, ("0.0.0.0") => {
-  console.log(`Addon running on port ${PORT}`);
 });
